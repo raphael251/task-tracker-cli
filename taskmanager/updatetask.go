8e73dfa2c88a0c985e1fa62ce7aee7e95f9d3f13
Raphael Passos <raphael251@hotmail.com>
2025-07-09 15:37:50 -0300

    fix: update task feature
    The update task function was not updating the object in the task manager array, it was just creating a new object with the updated data. Now it is updating just the object in the array directly, since it will be used to save the state in the DB

--- taskmanager/updatetask.go
+++ taskmanager/updatetask.go
@@ -3,15 +3,12 @@ package taskmanager
 import "time"
 
 func (tm *TasksManager) UpdateTask(id string, newDescription string) Task {
-	var updatedTask Task
-
 	for i := 0; i < len(tm.Tasks); i += 1 {
 		if tm.Tasks[i].Id == id {
-			updatedTask = tm.Tasks[i]
-			updatedTask.Description = newDescription
-			updatedTask.UpdatedAt = time.Now()
+			tm.Tasks[i].Description = newDescription
+			tm.Tasks[i].UpdatedAt = time.Now()
 
-			return updatedTask
+			return tm.Tasks[i]
 		}
 	}
 
